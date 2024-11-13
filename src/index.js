92f4e221c1ba86eff51f93cdf2eedd5888dfd82c
Paul Anderson <2762638a@student.gla.ac.uk>
2024-11-13 18:12:01 +0000

    chore: initialise react project
    Set up the project with webpack and babel and a gitignore

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+  return <h1>Hello from React in a Chrome Extension!</h1>;
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
