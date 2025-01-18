068728d2ad6b25183c41b3da5ec009c2f7582134
2772822H <2772822h@student.gla.ac.uk>
2025-01-18 18:59:54 +0000

    Fix: Changed the casing on the import for InputBar.test

--- src/tests/InputBar.test.js
+++ src/tests/InputBar.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
-import InputBar from '../components/InputBar/InputBar';
+import InputBar from '../components/Inputbar/InputBar';
 
 let mockAddTask;
 
