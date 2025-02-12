7005ae5c21085ad839427a50aeb29b2963c6819f
Mijuku <164499416+Keithlawlpy@users.noreply.github.com>
2025-02-12 20:41:07 +0000

    Fix merge conflicts in IndexInput and IndexList

--- src/components/IndexList/IndexList.js
+++ src/components/IndexList/IndexList.js
@@ -1,12 +1,7 @@
 import React from 'react';
-<<<<<<< HEAD
-
-const IndexList = ({ items }) => {
-=======
 import './IndexList.css'; 
 
 const IndexList = ({ items = []}) => {
->>>>>>> origin/main
   return (
     <ul>
       {items.map((item, index) => (
