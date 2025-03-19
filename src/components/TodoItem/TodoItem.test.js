5207ea5b37cccd10c05bea4813ff4be16fc3dd96
2772822H <2772822h@student.gla.ac.uk>
2025-03-19 15:58:26 +0000

    Chore: Moved TodoItem component test into its component directory

--- src/components/TodoItem/TodoItem.test.js
+++ src/components/TodoItem/TodoItem.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render, fireEvent, screen, waitFor } from "@testing-library/react";
-import TodoItem from "../components/TodoItem/TodoItem";
+import TodoItem from "./TodoItem";
 
 const mockTask = {
   id: 1,
