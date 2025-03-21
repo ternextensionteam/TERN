29f7509ac25298fb0dd4f1cbdd2e97cfe3a0ca92
Paul Anderson <2762638a@student.gla.ac.uk>
2025-03-21 16:23:05 +0000

    feat: add small pipeline to search engine
    add process term to search engine which lowercases and removes stop words from search term
    also replaces newline characters with spaces in crawler

--- src/background/searchEngine.js
+++ src/background/searchEngine.js
@@ -3,13 +3,19 @@ import { encode } from "he";
 import { removeAnchorLink, removeDuplicates } from "./text-utils.js";
 import { logToFile } from "../utils/Logger.js";
 
+const stopWords = ['i','me','my','myself','we','our','ours','ourselves','you','your','yours','yourself','yourselves','he','him','his','himself','she','her','hers','herself','it','its','itself','they','them','their','theirs','themselves','what','which','who','whom','this','that','these','those','am','is','are','was','were','be','been','being','have','has','had','having','do','does','did','doing','a','an','the','and','but','if','or','because','as','until','while','of','at','by','for','with','about','against','between','into','through','during','before','after','above','below','to','from','up','down','in','out','on','off','over','under','again','further','then','once','here','there','when','where','why','how','all','any','both','each','few','more','most','other','some','such','no','nor','not','only','own','same','so','than','too','very','s','t','can','will','just','don','should','now'];
+
 const INDEX_DESCRIPTOR = {
   fields: ["title", "content"], // Fields to index
   storeFields: ["title"], // Fields to return in search results
   idField: "id", // Field for unique identifier
+  processTerm: (term, _fieldName) =>
+    stopWords.includes(term) ? null : term.toLowerCase(),
   searchOptions: {
+    prefix: true, // Allow prefix matches
     boost: { title: 2 }, // Give higher weight to title matches
     fuzzy: 0.2, // Allow slight spelling variations
+    processTerm: (term) => term.toLowerCase()
   },
 };
 
